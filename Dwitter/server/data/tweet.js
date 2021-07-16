7124b6ee264aec7536d773bc02ac2c88eda0366d
raejun92 <cvcv1324@naver.com>
2021-07-16 13:48:58 +0900

    course by 13.4 model of MVC

---
+++ Dwitter/server/data/tweet.js
@@ -0,0 +1,53 @@
+let tweets = [ // 새로 추가되는 트위을 배열의 첫 부분에 넣어주기 위해 let으로 변경
+	{
+		id: '1',
+		text: 'hello dwitter',
+		createAt: new Date(),
+		name: 'juchoi',
+		username: 'bob',
+		url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+	},
+	{
+		id: '2',
+		text: 'I\'m two da',
+		createAt: new Date(),
+		name: 'mono',
+		username: 'ellie',
+	}
+];
+
+export function getAll() {
+	return tweets;
+}
+
+export function getAllByUsername(username) {
+	return tweets.filter(tweets => tweets.username === username);
+}
+
+export function getById(id) {
+	return tweets.find(tweet => tweet.id === id); // id가 없으면 undefined
+}
+
+export function create(text, username, name) {
+	const tweet = { // 새로운 트위를 만듦
+		id: Date.now().toString(), // db가 없어서 Date로 대체
+		text,
+		createAt: new Date(),
+		name,
+		username,
+	};
+	tweets = [tweet, ...tweets]; // 배열의 첫 번째를 새로운 tweet을 넣고 기존 tweets에 있던 tweet을 추가
+	return tweet;
+}
+
+export function update(id, text) {
+	const tweet = tweets.find(tweet => tweet.id === id); // tweets에서 변경하고자 하는 id를 찾음
+	if (tweet) {
+		tweet.text = text;
+	}
+	return tweet;
+}
+
+export function remove(id) {
+	tweets = tweets.filter(tweet => tweet.id !== id); // id인 tweet만 빼고 다시 tweets을 만듦
+}
--- Dwitter/server/route/tweets.js
+++ Dwitter/server/route/tweets.js
@@ -1,23 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
+import * as tweetRepository from '../data/tweet.js';
 
-let tweets = [ // 새로 추가되는 트위을 배열의 첫 부분에 넣어주기 위해 let으로 변경
-	{
-		id: '1',
-		text: 'hello dwitter',
-		createAt: new Date(),
-		name: 'juchoi',
-		username: 'bob',
-		url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-	},
-	{
-		id: '2',
-		text: 'I\'m two da',
-		createAt: new Date(),
-		name: 'mono',
-		username: 'ellie',
-	}
-];
 
 const router = express.Router();
 
@@ -27,8 +11,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
 	const username = req.query.username; // username이 없다면 undefined
 	const data = username  // 사용자에게 전달해줘야하는 데이터(사용자가 요청한 데이터)
-	? tweets.filter(tweet => tweet.username === username)
-	: tweets;
+	? tweetRepository.getAllByUsername(username)
+	: tweetRepository.getAll();
 	res.status(200).json(data);
 });
 
@@ -36,7 +20,7 @@ router.get('/', (req, res, next) => {
 // 원하는 id값의 tweet을 찾아 보여줌
 router.get('/:id', (req, res, next) => {
 	const id = req.params.id; // id를 받아옴
-	const tweet = tweets.find(tweet => tweet.id === id); // id가 없으면 undefined
+	const tweet = tweetRepository.getById(id);
 	if (tweet) {
 		res.status(200).json(tweet);
 	} else {
@@ -48,14 +32,7 @@ router.get('/:id', (req, res, next) => {
 // 새로운 tweet을 추가
 router.post('/', (req, res, next) => {
 	const {text, username, name} = req.body; // body안에서 text, username, name을 받아옴
-	const tweet = { // 새로운 트위를 만듦
-		id: Date.now().toString(), // db가 없어서 Date로 대체
-		text,
-		createAt: new Date(),
-		name,
-		username,
-	}
-	tweets = [tweet, ...tweets]; // 배열의 첫 번째를 새로운 tweet을 넣고 기존 tweets에 있던 tweet을 추가
+	const tweet = tweetRepository.create(text, username, name);
 	res.status(201).json(tweet);
 });
 
@@ -64,9 +41,8 @@ router.post('/', (req, res, next) => {
 router.put('/:id', (req, res, next) => {
 	const id = req.params.id; // 해당하는 tweet을 찾아
 	const text = req.body.text; // 내용만 업데이트함
-	const tweet = tweets.find(tweet => tweet.id === id); // tweets에서 변경하고자 하는 id를 찾음
+	const tweet = tweetRepository.update(id, text);
 	if (tweet) { // 해당 id가 있으면 text 변경
-		tweet.text = text;
 		res.status(200).json(tweet);
 	} else {
 		res.status(404).json({msesage: `Tweet id(${id}) not found`});
@@ -77,7 +53,7 @@ router.put('/:id', (req, res, next) => {
 // id에 해당하는 tweet을 제거(실제로 tweet을 제거하지 않고 해당 tweet만 뺀 배열을 만듦)
 router.delete('/:id', (req, res, next) => {
 	const id = req.params.id;
-	tweets = tweets.filter(tweet => tweet.id !== id); // id인 tweet만 빼고 다시 tweets을 만듦
+	tweetRepository.remove(id);
 	res.sendStatus(204); // 정상 삭제
 });
 
