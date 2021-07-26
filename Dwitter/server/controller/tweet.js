583baceae29d3839abcc7dccda2bb2f91f369345
raejun92 <cvcv1324@naver.com>
2021-07-26 11:08:42 +0900

    course by 16 Authorization

--- Dwitter/server/controller/tweet.js
+++ Dwitter/server/controller/tweet.js
@@ -27,16 +27,26 @@ export async function createTweet(req, res, next) {
 export async function updateTweet(req, res, next) {
 	const id = req.params.id; // 해당하는 tweet을 찾아
 	const text = req.body.text; // 내용만 업데이트함
-	const tweet = await tweetRepository.update(id, text);
-	if (tweet) { // 해당 id가 있으면 text 변경
-		res.status(200).json(tweet);
-	} else {
-		res.status(404).json({msesage: `Tweet id(${id}) not found`});
+	const tweet = await tweetRepository.getById(id);
+	if (!tweet) {
+		return res.sendStatus(404);
 	}
+	if (tweet.userId !== req.userId) {
+		return res.sendStatus(403);
+	}
+	const updated = await tweetRepository.update(id, text);
+	res.status(200).json(updated);
 }
 
 export async function deleteTweet(req, res, next) {
 	const id = req.params.id;
+	const tweet = await tweetRepository.getById(id);
+	if (!tweet) {
+		return res.sendStatus(404);
+	}
+	if (tweet.userId !== req.userId) {
+		return res.sendStatus(403);
+	}
 	await tweetRepository.remove(id);
 	res.sendStatus(204); // 정상 삭제
 }
